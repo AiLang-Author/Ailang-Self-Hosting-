73563541751073db8ef67c737b9766ca62eae781
Sean <smc.collins1977@gmail.com>
2025-12-26 00:06:48 -0500

    Add SCSL license, copyright headers, self-hosting compiler milestone - PrintNumber(Add(1,2)) working

--- ailang_compiler/modules/arithmetic_ops.py
+++ ailang_compiler/modules/arithmetic_ops.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2025 Sean Collins, 2 Paws Machine and Engineering. All rights reserved.
-
+#
+# Licensed under the Sean Collins Software License (SCSL). See the LICENSE file in the root directory of this project
+# for the full terms and conditions, including restrictions on forking, corporate use, and permissions for private/teaching purposes.
 # ailang_compiler/compiler/modules/arithmetic_ops.py
 """
 Arithmetic Operations Module - DEPTH-BASED REGISTER ALLOCATION
